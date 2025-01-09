0128491bfe1cd8efc93501812b3c1686366f4ca7
Cesar <cesar.hertz@icloud.com>
2025-01-09 16:46:30 +0100

    fix nav bar animmation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
+// import Sidebar from "../components/Sidebar";
 import "../styles/globals.css";
 
 import { ThemeProvider } from "next-themes";
 import { motion } from "framer-motion";
-import { ComponentProps, ReactNode } from 'react'
+import { ComponentProps, ReactNode, Suspense, lazy } from 'react'
+
+const Sidebar = lazy(() => import('../components/Sidebar'));
+
 
 type ExistingThemeProviderProps = ComponentProps<typeof ThemeProvider> & {children: ReactNode};
 const ThemeProviderExtended = (props: ExistingThemeProviderProps) => <ThemeProvider {...props}/>
 
 function MyApp({ Component, pageProps }) {
+
 
   return (
     <ThemeProviderExtended attribute="class">
@@ -17,13 +21,17 @@ function MyApp({ Component, pageProps }) {
       initial={{ y: 20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ ease: "easeInOut", duration: 0.75 }}
-      
       className="grid grid-cols-12 gap-6 px-5 my-14 lg:mb-0 md:mb-16 sm:px-20 md:px-32 lg:px-36 xl:px-48 ">
-        <div className="h-full col-span-12 p-4 text-base text-center bg-white
+
+        <motion.div 
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: "easeInOut", duration: 0.75 }}
+        className="h-full col-span-12 p-4 text-base text-center bg-white
          dark:bg-dark-500 border dark:border-orange
           lg:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark ">
           <Sidebar />
-        </div>
+        </motion.div>
         <div className="flex flex-col col-span-12 overflow-hidden
         border dark:border-orange
         bg-white shadow-custom-light dark:shadow-custom-dark rounded-2xl lg:col-span-9 dark:bg-dark-500">
