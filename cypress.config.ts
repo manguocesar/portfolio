a18146f670b413d570e7105df49ac406971c0d3f
Cesar <cesar.hertz@icloud.com>
2025-01-11 14:48:24 +0100

    cypress update config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000', // Your application's base URL
+    supportFile: 'cypress/support/index.js', // Path to the support file
+    specPattern: 'cypress/integration/**/*.cy.js', // Test file pattern
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
   },
 });
