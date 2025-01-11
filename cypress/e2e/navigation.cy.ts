ca71326bd6bd96f7a7baec9c97e0848e640d161d
Cesar <cesar.hertz@icloud.com>
2025-01-11 15:36:50 +0100

    add cypress fixture

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -6,22 +6,34 @@
 // Act: Perform actions like clicking or typing.
 // Assert: Verify the expected outcomes.
 
+
+
 describe("checkNavigation", () => {
+
   beforeEach(() => {
     cy.clearCookies();
     cy.visit("http://localhost:3000/");
   });
   it("visit the app 3 pages", () => {
-    cy.get('[data-test-id="cypress-nav-Resume"]', { timeout: 3000 })
+
+    cy.fixture('techno').as('technoData');
+    cy.get('@technoData').then((techno:any) => {
+      cy.get(`[data-test-id="cypress-nav-${techno.resume}"]`, { timeout: 3000 })
       .should("exist")
+      .and("be.visible")
       .click({ multiple: true });
+    });
+
+    
+
+
     cy.get('[data-test-id="cypress-nav-Projects"]')
       .should("exist")
       .click({ multiple: true });
     cy.get('[data-test-id="cypress-nav-About"]')
       .should("exist")
       .click({ multiple: true });
-      cy.get('[data-cy="change-theme"]').click();
+    cy.get('[data-cy="change-theme"]').click();
   });
   afterEach(() => {
     cy.visit("http://localhost:3000/");
