88619b25fe1c5e5d95482529db410d59001cd184
Cesar <cesar.hertz@icloud.com>
2025-02-17 14:23:53 +0100

    refactor: replace Icon with CircleCheckBig in ServiceCard for improved visual consistency

--- src/app/components/service-card.tsx
+++ src/app/components/service-card.tsx
@@ -1,12 +1,13 @@
 import { FunctionComponent } from 'react';
 import { Service } from '../types';
+import { CircleCheckBig } from 'lucide-react';
 
 const ServiceCard: FunctionComponent<{ service: Service }> = ({
-  service: { Icon, title, about },
+  service: { title, about },
 }) => {
   return (
     <div className="flex items-center space-x-4 p-2">
-      <Icon className="text-orange h-20 w-1/6 sm:h-24" />
+      <CircleCheckBig className="text-orange h-20 w-1/6 sm:h-24" />
       <div className="w-5/6">
         <h6 className="font-bold">{title}</h6>
         <p>{about}</p>
