9fb28f53f1c0fa4c35744095c3eaa4bfb6550b30
Cesar <cesar.hertz@icloud.com>
2025-02-17 14:57:22 +0100

    refactor: simplify layout styles by creating reusable CSS classes for body and content

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,6 @@ module.exports = {
     './components/**/*.tsx',
   ],
   darkMode: 'class',
-  theme: {
-    fontFamily: {
-      sans: ['ui-sans-serif', 'system-ui'],
-      kaushan: ['Kaushan Script'],
-    },
-    boxShadow: {
-      'custom-light': ' 0 0 10px #313131',
-      'custom-dark': '5px 5px 10px #0a0c0e , -5px -5px 10px #14161c',
-    },
-  },
   variants: {
     extend: {
       boxShadow: ['dark'],
