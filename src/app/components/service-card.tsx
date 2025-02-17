8383ad9ff3d0f7a1831d925ff6862147470821ce
Cesar <cesar.hertz@icloud.com>
2025-02-17 14:38:08 +0100

    refactor: update ServiceCard to use Icon prop from Service for improved flexibility

--- src/app/components/service-card.tsx
+++ src/app/components/service-card.tsx
@@ -1,13 +1,12 @@
 import { FunctionComponent } from 'react';
 import { Service } from '../types';
-import { CircleCheckBig } from 'lucide-react';
 
 const ServiceCard: FunctionComponent<{ service: Service }> = ({
-  service: { title, about },
+  service: { Icon, title, about },
 }) => {
   return (
     <div className="flex items-center space-x-4 p-2">
-      <CircleCheckBig className="text-orange h-20 w-1/6 sm:h-24" />
+      <Icon className="text-orange h-20 w-1/6 sm:h-24" />
       <div className="w-5/6">
         <h6 className="font-bold">{title}</h6>
         <p>{about}</p>
