9bb21868ab8dfaac9574f44670912c0498216572
Cesar <cesar.hertz@icloud.com>
2025-02-28 11:26:47 +0100

    refactor(i18n): optimize message import for improved readability

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -9,8 +9,9 @@ export default getRequestConfig(async ({ requestLocale }) => {
     locale = routing.defaultLocale;
   }
 
+  const messages = await import(`../../messages/${locale}.json`);
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default,
+    messages: messages.default,
   };
 });
